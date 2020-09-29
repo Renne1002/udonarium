50146d4487f5e37c2e3bd673e8045bd5ff922864
Renne1002 <dtl.134679@gmail.com>
2020-09-29 15:29:32 +0900

    シンラA1のトークン名も修正

--- src/app/component/megami-storage/megami-storage.component.ts
+++ src/app/component/megami-storage/megami-storage.component.ts
@@ -701,13 +701,13 @@ const FURUYONI_COMPONENTS = {
   ],
   'シンラ (A1)': [
     {
-      name: '計略トークン',
+      name: '計略トークン(神算)',
       front: './assets/furuyoni_commons_na/furuyoni_na/board_token/plan_blue.png',
       back: './assets/furuyoni_commons_na/furuyoni_na/board_token/plan_back.png',
       size: 1.5
     },
     {
-      name: '計略トークン',
+      name: '計略トークン(鬼謀)',
       front: './assets/furuyoni_commons_na/furuyoni_na/board_token/plan_red.png',
       back: './assets/furuyoni_commons_na/furuyoni_na/board_token/plan_back.png',
       size: 1.5
